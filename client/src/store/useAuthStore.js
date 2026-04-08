37c7ffe3766ec6b569479cf7a6f5805326650e6a
nandhubabu <nandhubabu@users.noreply.github.com>
2026-04-08 13:22:17 +0530

    feat: add checkAuth function to verify user session state in auth store

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -4,6 +4,19 @@ import { axiosInstance } from "../lib/axios";
 export const useAuthStore = create((set) => ({
     authUser: null, // Starts as null because no one is logged in
     isSigningUp: false,
+    isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({ authUser: res.data });
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
 
     // The Logic: The "Signup" function for the UI
     signup: async (data) => {
